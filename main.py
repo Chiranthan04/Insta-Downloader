366391a369a7a05cd90ac7780a1346757da8afea
chiranthan04 <chiranthangowda4224@gmail.com>
2022-11-28 20:11:19 +0000

    Can download Post with Profile pic

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import instaloader
 ig = instaloader.Instaloader()
 
 dp = input("Enter the Username: ")
-li = input("past the link: ")
-ig.download_profile(dp, profile_pic_only=True)
-
-
-ig.download_post(li,post=True)
+print("Downloading ...")
+ig.download_profile(dp, profile_pic_only=False)
+print("Download complited")
